b1e9f5059ec73c2cef29f89a00bc392f3eda1b26
gabriel <machad0.gabriel@outlook.com>
2022-06-19 23:40:14 -0300

    new key function added

--- nfzoom_proto.py
+++ nfzoom_proto.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+import pandas as pd
 
 browser = webdriver.Chrome()
 
@@ -16,5 +17,19 @@ browser.find_element_by_xpath('//*[@id="boxMainMenu"]/ul/li[2]/a').click()
 browser.find_element_by_xpath('//*[@id="coluna1"]/ul/li[8]/a').click()
 browser.find_element_by_xpath('//*[@id="coluna2"]/ul/li[5]/span/a/span[1]').click()
 time.sleep(15)
+
+# clicking on the CPF/CNPJ input form
+#browser.find_element_by_xpath('//*[@id="CpfCnpj"]').send_keys('1025678')
+
+# creating a data frame for the itaration
+cnpj_db = pd.read_excel('cnpjs.xlsx')
+
+def cnpj_iteration():
+    for cnpj in cnpj_db:
+        browser.find_element_by_xpath('//*[@id="CpfCnpj"]').send_keys(cnpj)
+        
+
+cnpj_iteration()
+#creating a function to iterate the copy/paste on the CNPJ dataframe 
 
 #browser.quit()
