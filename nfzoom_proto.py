6c1e3a222cda3de5b6f6a4c4ffbba56cfa22cbcb
gabriel <machad0.gabriel@outlook.com>
2022-06-22 11:12:11 -0300

    code refactor | cnpj_iteration function with problems

--- nfzoom_proto.py
+++ nfzoom_proto.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
-import time
 import pandas as pd
+import time
 
+# setting pre-requisites for the script
 browser = webdriver.Chrome()
+cnpj_db = pd.read_excel('cnpjs.xlsx')
+date_init = str(input('set initial date: '))
+date_final = str(input('set final date: '))
 
 # opening the browser and going to Agenci@Net:
 browser.get('https://www2.agencianet.fazenda.df.gov.br/')
@@ -12,24 +16,22 @@ time.sleep(15)
 browser.find_element_by_xpath('/html/body/div[5]/div/div[2]/a').click()
 time.sleep(15)
 
-# navagating through the menus
+# navagating through the menus 'Serviços -> Outros -> Chaves de Acesso/XML'
 browser.find_element_by_xpath('//*[@id="boxMainMenu"]/ul/li[2]/a').click()
 browser.find_element_by_xpath('//*[@id="coluna1"]/ul/li[8]/a').click()
 browser.find_element_by_xpath('//*[@id="coluna2"]/ul/li[5]/span/a/span[1]').click()
 time.sleep(15)
 
-# clicking on the CPF/CNPJ input form
-#browser.find_element_by_xpath('//*[@id="CpfCnpj"]').send_keys('1025678')
-
-# creating a data frame for the itaration
-cnpj_db = pd.read_excel('cnpjs.xlsx')
-
+# creating a function to iterate the copy/paste on the CNPJ dataframe 
 def cnpj_iteration():
     for cnpj in cnpj_db:
         browser.find_element_by_xpath('//*[@id="CpfCnpj"]').send_keys(cnpj)
-        
+        browser.find_element_by_xpath('//*[@id="DataInicio"]').send_keys(date_init)
+        browser.find_element_by_xpath('//*[@id="DataFim"]').send_keys(date_final)
+        browser.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div/div[2]/form/div[7]/div/button[2]').click()
+        time.sleep(10)
+        browser.find_element_by_xpath('/html/body/div[5]/div/div/div[2]/div/div[2]/div[1]/table/tbody/tr[1]/td[5]/a[2]/img').click
 
 cnpj_iteration()
-#creating a function to iterate the copy/paste on the CNPJ dataframe 
 
 #browser.quit()
